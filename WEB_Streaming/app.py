783bd9f714fc6b7ab8b80872a8c0d0c9a549fd36
j13tw <j13tw@yahoo.com.tw>
2018-09-22 02:20:45 +0800

    add RTMP auto generate uuid method

--- WEB_Streaming/app.py
+++ WEB_Streaming/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import requests
 import os, sys
+import uuid
 from datetime import datetime, timedelta	
 from flask import Flask, request, url_for, redirect, jsonify
 from flask import render_template
@@ -9,7 +10,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def rtmp():
-    return render_template('rtmp.html')
+    RTMP_HEADER = "rtmp://"
+    RTMP_IP = "10.0.0.174"
+    RTMP_PORT = "1935"
+    RTMP_URL_HEADER = "A0 - 0 - 0 - 0 - "
+    IPCAM_ACCOUNT = "admin"
+    IPCAM_PASSWORD = "888888"
+
+    generate_uuid = uuid.uuid4()
+    user_uuid = str(generate_uuid).upper().replace('-', '')
+    rtmp_url = RTMP_HEADER + RTMP_IP + ":" + RTMP_PORT + "/" + RTMP_URL_HEADER + user_uuid + " - " + IPCAM_ACCOUNT + " - " + IPCAM_PASSWORD
+    print(rtmp_url)
+    return render_template('rtmp.html', rtmp_url = rtmp_url)
 
 if __name__ == '__main__':
 #	app.run(debug = True)
